52fed35c1311d1a4acb05c070c71ba4ecf6f7241
likejean <popachs@yahoo.com>
2020-03-26 03:09:34 -0700

    Updates

---
+++ precache-manifest.bdba0407d2d015a9c7fd669ea7f39987.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff1f070650c4ceb06e42305277fbc028",
+    "url": "/homework-2/index.html"
+  },
+  {
+    "revision": "7a279209669e40ec2048",
+    "url": "/homework-2/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "15ec0d6a1990c65ec75f",
+    "url": "/homework-2/static/css/main.3be0d065.chunk.css"
+  },
+  {
+    "revision": "7a279209669e40ec2048",
+    "url": "/homework-2/static/js/2.f0a63919.chunk.js"
+  },
+  {
+    "revision": "76fd5ab146b0d8fb7b899b4b882f747e",
+    "url": "/homework-2/static/js/2.f0a63919.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15ec0d6a1990c65ec75f",
+    "url": "/homework-2/static/js/main.c693e776.chunk.js"
+  },
+  {
+    "revision": "c5fa49ae0bd7371fa44b",
+    "url": "/homework-2/static/js/runtime-main.d2e3b684.js"
+  },
+  {
+    "revision": "9f08a9dca4da620ac173c515ef26d2bb",
+    "url": "/homework-2/static/media/earth.9f08a9dc.png"
+  }
+]);
